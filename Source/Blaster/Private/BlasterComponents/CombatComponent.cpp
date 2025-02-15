56b1514e8f249076218315549d68d7c70b3584cf
robkohout <robkohout@hotmail.com>
2025-02-14 21:52:02 -0800

    Allowing Weapon Fire

--- Source/Blaster/Private/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/Private/BlasterComponents/CombatComponent.cpp
@@ -112,7 +112,7 @@ void UCombatComponent::ServerFire_Implementation(const FVector_NetQuantize& Trac
 void UCombatComponent::MulticastFire_Implementation(const FVector_NetQuantize& TraceHitTarget)
 {
 	if (EquippedWeapon == nullptr) return;
-	if (Character)
+	if (Character && CombatState == ECombatState::ECS_Unoccupied)
 	{
 		Character->PlayFireMontage(bAiming);
 		EquippedWeapon->Fire(TraceHitTarget);
@@ -166,6 +166,10 @@ void UCombatComponent::FinishReloading()
 	{
 		CombatState = ECombatState::ECS_Unoccupied;	
 	}
+	if (bFireButtonPressed)
+	{
+		Fire();
+	}
 }
 
 void UCombatComponent::HandleReload()
@@ -186,6 +190,12 @@ void UCombatComponent::OnRep_CombatState()
 	{
 		case ECombatState::ECS_Reloading:
 			HandleReload();
+			break;
+		case ECombatState::ECS_Unoccupied:
+			if (bFireButtonPressed)
+			{
+				Fire();
+			}
 			break;
 	}
 }
@@ -371,7 +381,7 @@ void UCombatComponent::ServerSetAiming_Implementation(bool bIsAiming)
 bool UCombatComponent::CanFire()
 {
 	if (EquippedWeapon == nullptr) return false;
-	return !EquippedWeapon->IsEmpty() || !bCanFire;
+	return !EquippedWeapon->IsEmpty() && bCanFire && CombatState == ECombatState::ECS_Unoccupied;
 }
 
 void UCombatComponent::OnRep_CarriedAmmo()
