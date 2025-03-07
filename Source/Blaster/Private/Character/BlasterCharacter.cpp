26007eb22376da89040f1fc816a9695b7efcd4dc
robkohout <robkohout@hotmail.com>
2025-03-06 22:02:31 -0800

    Grenade Launcher

--- Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -152,6 +152,9 @@ void ABlasterCharacter::PlayReloadMontage()
 		case EWeaponType::EWT_SniperRifle:
 			SectionName = FName("Rifle");
 			break;
+		case EWeaponType::EWT_GrenadeLauncher:
+			SectionName = FName("Rifle");
+			break;
 		default:
 			break;
 		}
