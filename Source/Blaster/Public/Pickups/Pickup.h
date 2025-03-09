433a1f3be0a11f3f043f25c0d04f5751c87a446a
robkohout <robkohout@hotmail.com>
2025-03-08 18:35:15 -0800

    Pickup Class

---
+++ Source/Blaster/Public/Pickups/Pickup.h
@@ -0,0 +1,43 @@
+// Copyright Rob Kohout
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+class USphereComponent;
+class USoundCue;
+
+UCLASS()
+class BLASTER_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APickup();
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+	
+private:
+	UPROPERTY(EditAnywhere)
+	USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	USoundCue* PickupSound;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+};
