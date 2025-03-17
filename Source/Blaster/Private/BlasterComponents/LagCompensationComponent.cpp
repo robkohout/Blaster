559af5fba2ba8001e3582e9b1ca6fcb6ff2ff1fc
robkohout <robkohout@hotmail.com>
2025-03-16 17:28:32 -0700

    Lag Compensation Component

---
+++ Source/Blaster/Private/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,23 @@
+// Copyright Rob Kohout
+
+
+#include "BlasterComponents/LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	
+}
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
